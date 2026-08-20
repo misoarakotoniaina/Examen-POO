22e34229486b18d7297ab96ec4c879af4e3a8d0e
andy_bryan <hei.bryan.46@gmail.com>
2026-08-20 14:08:17 +0300

    fix : mise en place de lombok

--- src/main/java/Modele/Product.java
+++ src/main/java/Modele/Product.java
@@ -1,10 +1,22 @@
 package Modele;
 
 import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 public class Product {
     private String id;
     private String name;
     private String description;
     private BigDecimal unitPrice;
+
+    public Product(String id, String name, String description, BigDecimal unitPrice) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.unitPrice = unitPrice;
+    }
 }
--- src/main/java/Modele/StockMovement.java
+++ src/main/java/Modele/StockMovement.java
@@ -1,13 +1,26 @@
 package Modele;
 
 import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 public class StockMovement {
-        private enum movementType{
+    private enum movementType{
             IN, OUT
         }
     private String id;
     private Instant createdAt;
     private movementType MovementType;
     private int quantity;
+
+    public StockMovement(String id, Instant createdAt, movementType movementType, int quantity) {
+        this.id = id;
+        this.createdAt = createdAt;
+        MovementType = movementType;
+        this.quantity = quantity;
+    }
+
 }
